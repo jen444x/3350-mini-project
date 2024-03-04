c9dbf20b4a43344ec600f72d8ddb4bb2f8a6b814
Hildur Jonasdottir <hjonasdottir@odin.cs.csubak.edu>
2024-03-04 13:55:39 -0800

    shows how many tasks are due today

--- task_manager.cpp
+++ task_manager.cpp
@@ -1,7 +1,7 @@
 #include <iostream> 
 #include <fstream>
 #include <bits/stdc++.h>
-#include <string.h>
+#include <regex>
 
 using namespace std;
 
@@ -11,163 +11,208 @@ class TaskManager
         struct Task_attributes {
             string name;
             string due_date;
-            // int priority;  
+            int priority;  // 1 - high priority, 2 - med priority, 3 - low priority                                           
+            // string description / notes?
         };
         vector<Task_attributes> vec;
-        fstream my_file;
+        fstream my_file;                                
     public:
-        TaskManager() {   
-            // Opening the file for reading & writing.
-            my_file.open("file.txt", ios::in );
-            if (!my_file) {
-                // Create file
-                my_file.open("file.txt", ios::out);
-                if (!my_file) {
-                    cout << "Error creating file!\n\n";
-                    return;
-                }
-            }
-            else {
-                // Load tasks from file into vector
-                string task_name, task_date;
-                Task_attributes data;
-
-                while (getline (my_file, task_name) && getline(my_file, task_date)) {
-                    data.name = task_name;
-                    data.due_date = task_date;
-                    vec.push_back(data);
-                  //  cout << vec.back().name << " " << vec.back().due_date << endl;
-                }
-            }     
-            my_file.close();
-        }
-        ~TaskManager() {
-            // Save tasks from vector to file 
+    TaskManager() {  
+        // Opening the file for reading & writing.
+        my_file.open("file.txt", ios::in );
+        if (!my_file) {
+            // Create file
             my_file.open("file.txt", ios::out);
-            if (my_file.is_open()) {
-                for (auto& a : vec) {
-                    my_file << a.name << "\n" << a.due_date << "\n";
-                }
-                cout << "Data saved succesfully.\n\n";
-
-                my_file.close(); 
-            }
-            else {
-                cout << "File is not open. Task not added.\n\n";
+            if (!my_file) {
+                cout << "Error creating file!\n\n";
+                return;
             }
         }
-
-        /*  static bool compare_dates (string date1, string date2) {
-            return (date1 < date2);  // soonest dates go first 
-            } */
-
-        void add_task(string name, string due_date)
-        {
+        else {
+            // Load tasks from file into vector
+            string task_name, task_date, priority_str;
+            int priority;
             Task_attributes data;
-            data.name = name;
-            data.due_date = due_date;
-            // add task to vector
-            vec.push_back(data);
 
-            cout << "\nTask has been added.\n\n";
-
+            while (getline(my_file, task_name) && getline(my_file, task_date) && getline(my_file, priority_str)) {
+                data.name = task_name;
+                data.due_date = task_date;
+                priority = stoi(priority_str);
+                data.priority = priority;
+                vec.push_back(data);
+            }
+        }     
+        my_file.close();
+    }
+    ~TaskManager() {
+        // Save tasks from vector to file 
+        my_file.open("file.txt", ios::out);
+        if (my_file.is_open()) {
             for (auto& a : vec) {
-                cout << "not sorted:\n";
-                cout << a.name << " " << a.due_date << endl;
+                my_file << a.name << "\n" << a.due_date << "\n" << a.priority << "\n";
             }
+            cout << "Data saved succesfully.\n\n";
 
-            // sort data when something is added
-
-            /*    sort(vec.begin(), vec.end(), compare_dates);
-                  cout << "sorted:\n";
-                  for (auto& a : vec) {
-                  cout << a.name << " " << a.due_date << endl;
-                  } */
-
+            my_file.close(); 
         }
-        bool edit_task(string n)
-        {
-            char input;
-            string new_name, new_ddate;
-            // find task
-            int size = vec.size();
-            for (int i = 0; i < size; i++) {
-                if (vec[i].name == n) {
-                    do {
-                        cout << "Task found: " << vec[i].name << " due on " << vec[i].due_date << "\n\n"
-                            "Press 'n' to edit the name of the task.\n"
-                            "Press 'd' to edit the due date of the task.\n";
-                        cin >> input;
-                        cin.ignore();
+        else {
+            cout << "File is not open. Task not added.\n\n";
+        }
+    }
 
-                        if (input == 'n') {
-                            cout << "New name: ";
-                            cin >> new_name;
+    void add_task(string name, string due_date, int priority)
+    {
+        Task_attributes data;
+        data.name = name;
+        data.due_date = due_date;
+        data.priority = priority;
+        // add task to vector
+        vec.push_back(data);
+        
+        cout << "\nTask has been added.\n\n";
 
-                            vec[i].name = new_name;
-                            return true;
-                        }
-                        else if (input == 'd') {
-                            cout << "New due date (year:month:day | example: 2024:03:04): ";
+        // sort data
+        sort(vec.begin(), vec.end(), compare_dates);
+    }
+   bool edit_task(string n)
+    {
+        char input;
+        string new_name, new_ddate;
+        int new_priority;
+        bool found;
+
+        // find task
+        int size = vec.size();
+        for (int i = 0; i < size; i++) {
+            if (vec[i].name == n) {
+                do {
+                    found = true;
+                    cout << "Task found: " << vec[i].name << " due on " << vec[i].due_date << "\n\n"
+                            "Press 'n' to edit the name.\n"
+                            "Press 'd' to edit the due date.\n"
+                            "Press 'p' to edit the priority.\n"
+                            "Press 'x' to continue looking without deleting.\n";;
+                    cin >> input;
+                    cin.ignore();
+
+                    if (input == 'n') {
+                        cout << "New name: ";
+                        cin >> new_name;
+
+                        vec[i].name = new_name;
+                        return true;
+                    }
+                    else if (input == 'd') {
+                        do {
+                            cout << "New due date: ";
                             cin >> new_ddate;
+                        } while (!valid_date_format(new_ddate));
 
-                            vec[i].due_date = new_ddate;
-                            return true;
-                        }
-                        else {
-                            cout << "Input not recognised.\n";
-                        }
-                    } while (input != 'n' || input != 'd');
-                }
+                        vec[i].due_date = new_ddate;
+
+                        // sort data
+                        sort(vec.begin(), vec.end(), compare_dates);
+                        return true;
+                    }
+                    else if (input == 'p') {
+                        do {
+                            cout << "New priority level (1, 2, or 3): ";
+                            cin >> new_priority;
+                        } while (new_priority > 3 || new_priority < 1);
+
+                        vec[i].priority = new_priority;
+
+                        // sort data
+                        sort(vec.begin(), vec.end(), compare_dates);
+
+                    }
+                    else if (input == 'x') {
+                        found == true;
+                    }
+                    else {
+                        cout << "Input not recognized.\n";
+                    }
+                 } while (input != 'x');
             }
-            return false;
-        } 
-        bool delete_task(string n)
-        {   
-            char input;
-            // find task
-            int size = vec.size();
-            for (int i = 0; i < size; i++) {
-                if (vec[i].name == n) {
-                    // confirm its the correct task
-                    do {
-                        cout << "Task found: " << vec[i].name << " due on " << vec[i].due_date << "\n\n"
+        }
+        if (found == false) {
+            cout << n << " was not found.\n\n";
+        }
+        return false;
+    } 
+    bool delete_task(string n)
+    {   
+        char input;
+        bool found = false;
+
+        // find task
+        int size = vec.size();
+        for (int i = 0; i < size; i++) {
+            if (vec[i].name == n) {
+                // confirm its the correct task
+                do {
+                    found = true;
+                    cout << "Task found: " << vec[i].name << " due on " << vec[i].due_date << "\n\n"
                             "Press 'c' to confirm deletion of this task.\n"
                             "Press 'x' to continue looking without deleting.\n";
-                        cin >> input;
-                        cin.ignore();
+                    cin >> input;
+                    cin.ignore();
 
-                        // if it's not, continue to look for it
-                        if (input == 'c') {
-                            // delete task
-                            vec.erase(vec.begin()+i);
-                            return true;
-                        }
-                    } while (input != 'x');
-                }
+                    // if it's not, continue to look for it
+                    if (input == 'c') {
+                        // delete task
+                        vec.erase(vec.begin()+i);
+                        return true;
+                    }
+                } while (input != 'x');
             }
-            return false;
         }
-        int tasks_due_today(string date)
-        {
-            int count = 0;
-            int size = vec.size();
-            for (int i = 0; i < size; i++) {
-                string ddate = vec[i].due_date;
-                if (strcmp(ddate.c_str(), date.c_str()) == 0) {
-                    count++;
-                }
+        if (found == false) {
+            cout << "\n" << n << " was not found.\n\n";
+        }
+        return false;
+    }
+    void show_tasks()
+    {
+        int size = vec.size();
+        for (int i = 0; i < size; i++) {
+            cout << vec[i].name << " - Due on " << vec[i].due_date << endl;
+        }
+        cout << endl;
+    }
+    static bool compare_dates (Task_attributes& task1, Task_attributes& task2) {
+        if (task1.due_date < task2.due_date) {
+            return true;
+        }
+        
+        if (task1.due_date == task2.due_date) {
+            return (task1.priority < task2.priority);
+        }
+
+        return false;  // soonest dates go first
+    }
+    bool valid_date_format (string date) {
+        regex pattern("\\d{4}:\\d{2}:\\d{2}");    // date format that will allow '/'
+        bool valid_format;
+        valid_format = regex_match(date, pattern);  // compares user date format to desired format 
+        if (!valid_format) {
+            cout << "Invalid date format. Please use the following format:\n"
+                    "- yyyy:mm:dd\n\n";
+        }
+        return valid_format;
+    }
+    int tasks_due_today(string today)
+    {
+        int count = 0;
+        int size = vec.size();
+        for (int i = 0; i < size; i++) {
+            string date = vec[i].due_date;
+            if(strcmp(date.c_str(), today.c_str()) == 0) {
+                count++;
             }
-            return count;
         }
-        void show_tasks()
-        {
-            int size = vec.size();
-            for (int i = 0; i < size; i++) {
-                cout << vec[i].name << " - Due on " << vec[i].due_date << endl;
-            }
-            cout << endl;
-        }
+        return count;
+    }
 };
 
 int main() {
@@ -188,8 +233,7 @@ int main() {
     num = manager.tasks_due_today(buf);
 
     cout << "------- WELCOME TO TASK MANAGER -------" << endl << endl;
-    cout << "Date: " << buf << endl;
-    cout << "You have " << num << " tasks due today - ";
+    cout << buf << " - You have " << num << " tasks due today - ";
     if (num == 0) 
         cout << "YAY do something other than homework today ;D" << endl << endl;
     else
@@ -204,22 +248,29 @@ int main() {
         cout << "q: quit" << endl << endl;
         cin >> input;
         cin.ignore();
-
+        
         if (input == 'a')
         {
+            cout << endl;
             cout << "Name of task: ";
             getline(cin, name);
+            do {
+                cout << "Due date of task: ";
+                getline(cin, due_date);
+            } while (!manager.valid_date_format(due_date));
+            do {
+                cout << "Priority level (Choose 1, 2, or 3): ";
+                cin >> priority;
+                cin.ignore();
+            } while (priority > 3 || priority < 1);
 
-            cout << "Due date of task (year:month:day | example: 2024:03:04): ";
-            getline(cin, due_date);
-
-            manager.add_task(name, due_date);
+            manager.add_task(name, due_date, priority);
         }
         else if (input == 'e')
         {
+            cout << endl;
             cout << "Name of task that needs editing: ";
             getline(cin, name);
-
 
             if(manager.edit_task(name)) {
                 cout << "Succesfully edited.\n\n";
@@ -230,6 +281,7 @@ int main() {
         }
         else if (input == 'd')
         {
+            cout << endl;
             cout << "Name of task to delete: ";
             getline(cin, name);
             if (manager.delete_task(name)) {
@@ -241,10 +293,14 @@ int main() {
         }
         else if (input == 's')
         {
+            cout << endl;
             cout << "Here are all your current tasks: " << endl << endl;
             manager.show_tasks();
         }   
-    } while (input != 'q');
+        else if (input != 'q') {
+            cout << "\nInput not recognized.\n";
+        }
+} while (input != 'q');
 
     return 0;
 }
